84e0a3cadc9823d883c31b54baaa54b4bea75b84
gameprix <Wahyudikdl92@gmail.com>
2025-12-11 19:05:50 +0700

    Update export-mp3.js

--- engine/export-mp3.js
+++ engine/export-mp3.js
@@ -1,5 +1,13 @@
 export const ExportMP3 = {
-    export(engine) {
-        alert("Exporting MP3 (LAME encoder)...");
+  async export(engine) {
+    if(!window.Lame) {
+      await new Promise(res => {
+        const s = document.createElement('script');
+        s.src = 'https://cdn.jsdelivr.net/gh/zhuker/lamejs/lame.min.js';
+        s.onload = res;
+        document.body.appendChild(s);
+      });
     }
+    alert("Export MP3 applied (LAME placeholder)");
+  }
 };
